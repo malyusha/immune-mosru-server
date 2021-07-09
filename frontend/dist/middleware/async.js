dcdc005a1e07fc0da212f21561fedc73852bb49c
Игорь Малюк <i.malyuk@corp.mail.ru>
2021-07-09 16:32:26 +0300

    lru add

--- frontend/dist/middleware/async.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function asyncWrap(fn) {
-    return (req, res, next) => {
-        return Promise.resolve(fn(req, res, next)).catch(next);
-    };
-}
-exports.default = asyncWrap;
